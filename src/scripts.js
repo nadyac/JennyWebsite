aadf7bd1c0a737168445995fb18283c520a09e07
nadyac <nadyacpena@gmail.com>
2014-08-23 22:31:22 -0400

    improved JS

--- src/scripts.js
+++ src/scripts.js
@@ -5,10 +5,11 @@ function popup() {
     alert("Hello World");
 }
 
-$(function getWinSize() {
+$(window).resize(function getWinSize() {
     //Call variable to hold window size
     "use strict";
     var win_Width = $(window).width(),
         win_Height = $(window).height();
-    alert("Width: " + win_Width + " Height: " + win_Height);
+    //alert("Width: " + win_Width + " Height: " + win_Height);
+    
 });
